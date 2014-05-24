dd034fe690b29bb9e1b2f2106e2940369e297db9
shogunsea <shogunsea@foxmail.com>
2014-05-24 19:23:11 +0800

    add bootstrap theme, add invalid path redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  def not_found
+    # May be log the invalid requests
+    render 'layouts/hehe'
+    # raise ActionController::RoutingError.new('Not Found')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :articles
+
+
+  root 'articles#index'
+
+
+  get '*invalid_path' => 'application#not_found'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
